a069319a1eae14727e2f4454c5f22d90ee17f8e9
jamesc16 <jmacook16@gmail.com>
2024-04-23 13:28:23 +0100

    Create navbar as component

---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="bg-slate-400 w-[20%] h-[100vh]">
+      <img src="jc-logo-draft1.png" alt="jc-logo" className="h-9 w-9 m-2" />
+      <h3>James Cook</h3>
+      <h4>Software Developer</h4>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import Navbar from "../../components/navbar";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col bg-slate-400 w-[20%] h-[100vh]">
+      <Navbar />
+      {/* <div className="bg-slate-400 w-[15%] h-[100vh]">
         <img src="jc-logo-draft1.png" alt="jc-logo" className="h-9 w-9 m-2" />
         <h3>James Cook</h3>
         <h4>Software Developer</h4>
-      </div>
+      </div> */}
     </main>
   );
 }
